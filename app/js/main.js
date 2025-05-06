d7d18957bd63f894053b8defc942f20ffd6e7a1f
sergey-komar <kursev81@yandex.ru>
2025-05-06 13:02:24 +0300

    home

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function () {
+    $('.heading-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    });
 
+    $('.sellers-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    });
+
+
+    $('.brand-slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        dots: true
+    });
 })
